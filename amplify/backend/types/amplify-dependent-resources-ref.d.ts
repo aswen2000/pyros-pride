6dd77b1e67ae85c7fb735940a97b2ee42ca95574
aswen2000 <aswen2000@gmail.com>
2021-12-27 12:09:37 -0600

    storage, post/list products

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "product": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
