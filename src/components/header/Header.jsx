8c623762582e82e1bb95e8865b776162536528d8
aswen2000 <aswen2000@gmail.com>
2021-12-29 18:01:06 -0600

    logo and links working

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,38 @@
 import React from "react";
+import './Header.css';
 import { Link } from "react-router-dom";
 import facebookLogo from '../../images/f_logo_RGB-Blue_58.png'
 import flashingThunderLogo from '../../images/long-logo.png'
 import { Mailto } from '../index'
 import {Home, About} from '../../pages/index'
-
+import Grid from '@mui/material/Grid';
 
 const Header = () => {
     return (
+        <div className="outer_wrapper">
+            <Grid xs={12} sm={12} md={5} lg={5} xl={5} className="logo_grid">
+                <Link to="/" replace={true}>
+                    <div className="wrap">
+                        <img className="header_logo" src={flashingThunderLogo} alt="Flashing Thunder Logo"></img>
+                    </div>
+                </Link> 
+            </Grid>
+            <Grid xs={12} sm={12} md={7} lg={7} xl={7} className="link_grid">
+                <Link to="/" className="navbar_item" replace={true}>Home</Link>
+                <Link to="/about" className="navbar_item" replace={true}>About</Link>
+                <Link to="/products" className="navbar_item" replace={true}>Products</Link>
+                <Link to="/thunderwear" className="navbar_item" replace={true}>Thunder Wear</Link>
+                <Link to="/locations" className="navbar_item" replace={true}>Locations</Link>
+                <Link to="/contact" className="navbar_item" replace={true}>Contact</Link>
+            </Grid>
+        </div>
+    );
+}
+
+export default Header;
+
+
+/*
         <div className="header">
             <div className="header-top">
                 <div className="header-top__logo">
@@ -18,12 +43,12 @@ const Header = () => {
                 <div className="header-top__navbar">
                     <div className="header-top__navigation">
                         <div className="navbar">
-                            <Link to="/" className="navbar-item" replace={true}>Home</Link>
-                            <Link to="/about" className="navbar-item" replace={true}>About</Link>
-                            <Link to="/products" className="navbar-item" replace={true}>Products</Link>
-                            <Link to="/thunderwear" className="navbar-item" replace={true}>Thunder Wear</Link>
-                            <Link to="/locations" className="navbar-item" replace={true}>Locations</Link>
-                            <Link to="/contact" className="navbar-item" replace={true}>Contact</Link>
+                            <Link to="/" className="navbar_item" replace={true}>Home</Link>
+                            <Link to="/about" className="navbar_item" replace={true}>About</Link>
+                            <Link to="/products" className="navbar_item" replace={true}>Products</Link>
+                            <Link to="/thunderwear" className="navbar_item" replace={true}>Thunder Wear</Link>
+                            <Link to="/locations" className="navbar_item" replace={true}>Locations</Link>
+                            <Link to="/contact" className="navbar_item" replace={true}>Contact</Link>
                         </div>
                     </div>
                     <hr className="header-top__seperator" />
@@ -44,7 +69,4 @@ const Header = () => {
                 </div>
             </div>
         </div>
-    );
-}
-
-export default Header;
+*/
