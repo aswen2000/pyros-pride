c89dc7b412996e0aba1155ac30394dcef57f7dad
aswen2000 <aswen2000@gmail.com>
2022-01-02 14:48:35 -0600

    initial product card

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,26 +1,43 @@
 import React from "react";
-import './Product.css';
+import styles from './Product.css';
 import YouTube from 'react-youtube';
 
-const Product = (product_name, product_number, description, image, link) => {
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const Product = ({product_name, product_number, description, image, link}) => {
+    // const opts = {
+    //     height: '270',
+    //     width: '480'
+    // };
+
     return (
-        <div>
-            <YouTube videoId={link.link}/>
-        </div>
+        <Card sx={{ maxWidth: 550 }}>
+            <CardMedia
+            component="img"
+            image={image}
+            alt="display img"
+            />
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {product_name}
+                </Typography>
+                <Typography gutterBottom variant="h7" component="div">
+                    {product_number}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ mt: 2 }}>
+                    {/* <YouTube videoId={link} opts={opts}/> */}
+                    {description}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
 
+
+
 export default Product;
-
-
-/*
-  product_number: String! #maybe should'nt be required?
-  product_name: String
-  packing_num: Int
-  box_per_case: Int
-  product_per_box: Int
-  description: String
-  image: String
-  video_link: String
-}
-*/
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,21 @@
+import Product from "../../components/product/Product";
+// import facebookLogo from '../../images/f_logo_RGB-Blue_58.png'
+import PPSh from "../../images/PPSh-41_from_soviet.jpg"
+
 const Products = () => {
 
     return (
-        <div className="products">
-            This is Products
+        <div>
+            <div className="product_card">
+                <Product
+                    product_name={"Arbitrary Example"}
+                    product_number={"PP-SH41"} 
+                    description={"This is an example of a description, it has great pops and bangs really quickly"}
+                    image={PPSh}
+                    link={"FAIyQ5yqVu8"}/>
+            </div>
         </div>
+
     );
 }
 
