1d52730f113bb66c5b799d963c02000c66d5c706
aswen2000 <aswen2000@gmail.com>
2022-01-10 20:42:19 -0600

    formatting for admin card preview

--- src/components/adminProduct/AdminProduct.jsx
+++ src/components/adminProduct/AdminProduct.jsx
@@ -12,6 +12,7 @@ import {
     AccordionDetails,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { styled } from "@mui/material/styles";
 import React, { useEffect } from "react";
 import "./AdminProduct.css";
 
@@ -31,23 +32,32 @@ const AdminProduct = ({ handleDelete, product }) => {
         video_link,
     } = product;
 
+    const CardContentNoPadding = styled(CardContent)(`
+        padding: 0;
+        &:last-child {
+        padding-bottom: 0;
+        }
+    `);
+
     return (
         <Card className="card" sx={{ maxWidth: 475 }}>
             <CardMedia sx={{ maxHeight: 400, maxWidth: "auto" }} component="img" image={image} alt="display img" />
-            <CardContent>
-                <Accordion>
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <CardContentNoPadding sx={{ pb: 0, pt: 0 }} className="card_content">
                         <Typography>{product_name}</Typography>
-                        <Typography sx={{ color: "text.secondary" }}>{product_number}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography variant="body2" color="text.secondary">
-                            {description}
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-            </CardContent>
-            <Button onClick={() => handleDelete(id)}>delete btn</Button>
+                        <Typography sx={{ color: "text.secondary", pb: 0 }}>{product_number}</Typography>
+                    </CardContentNoPadding>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* <Button onClick={() => handleDelete(id)}>delete btn</Button> */}
         </Card>
     );
 };
