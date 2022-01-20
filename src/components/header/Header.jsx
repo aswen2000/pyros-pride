37a7accfe3d3b1b320f274e4dcf90c0aa67551a3
aswen2000 <aswen2000@gmail.com>
2022-01-19 20:32:46 -0600

    slightly fix header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,44 +10,46 @@ import { Mailto } from "../index";
 const Header = () => {
     return (
         <div className="outer_wrapper">
-            <Grid xs={12} sm={12} md={5} lg={5} xl={5} className="logo_grid">
-                <Link to="/" replace>
-                    <img className="header_logo" src={flashingThunderLogo} alt="Flashing Thunder Logo" />
-                </Link>
-            </Grid>
-            <Grid xs={12} sm={12} md={7} lg={7} xl={7} className="link_grid">
-                <Link to="/" className="navbar_item" replace>
-                    Home
-                </Link>
-                <Link to="/about" className="navbar_item" replace>
-                    About
-                </Link>
-                <Link to="/products" className="navbar_item" replace>
-                    Products
-                </Link>
-                <Link to="/thunderwear" className="navbar_item" replace>
-                    Thunder Wear
-                </Link>
-                <Link to="/locations" className="navbar_item" replace>
-                    Locations
-                </Link>
-                <Link to="/contact" className="navbar_item" replace>
-                    Contact
-                </Link>
+            <Grid container>
+                <Grid xs={12} sm={12} md={12} lg={5} xl={5} className="logo_grid">
+                    <Link to="/" replace>
+                        <img className="header_logo" src={flashingThunderLogo} alt="Flashing Thunder Logo" />
+                    </Link>
+                </Grid>
+                <Grid xs={12} sm={12} md={12} lg={7} xl={7} className="link_grid">
+                    <Link to="/" className="navbar_item" replace>
+                        Home
+                    </Link>
+                    <Link to="/about" className="navbar_item" replace>
+                        About
+                    </Link>
+                    <Link to="/products" className="navbar_item" replace>
+                        Products
+                    </Link>
+                    <Link to="/thunderwear" className="navbar_item" replace>
+                        Thunder Wear
+                    </Link>
+                    <Link to="/locations" className="navbar_item" replace>
+                        Locations
+                    </Link>
+                    <Link to="/contact" className="navbar_item" replace>
+                        Contact
+                    </Link>
 
-                <hr className="header_divider" />
+                    <hr className="header_divider" />
 
-                <div className="header_contact_wrapper">
-                    <div className="header_phone">641-732-5558</div>
+                    <div className="header_contact_wrapper">
+                        <div className="header_phone">641-732-5558</div>
 
-                    <div className="header_mailto">
-                        <Mailto label="katie@flashingthunder.com" mailto="mailto:katie@flashingthunder.com" />
+                        <div className="header_mailto">
+                            <Mailto label="katie@flashingthunder.com" mailto="mailto:katie@flashingthunder.com" />
+                        </div>
+
+                        <a href="https://www.facebook.com/iowaflashingthunder/" target="_blank" rel="noreferrer">
+                            <img className="facebook_logo" src={facebookLogo} alt="Facebook Logo" />
+                        </a>
                     </div>
-
-                    <a href="https://www.facebook.com/iowaflashingthunder/" target="_blank" rel="noreferrer">
-                        <img className="facebook_logo" src={facebookLogo} alt="Facebook Logo" />
-                    </a>
-                </div>
+                </Grid>
             </Grid>
         </div>
     );
